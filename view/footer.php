d0878972ef3455a7a14061dfd9749e82b841e6a4
fasholatolulope52@gmail.com <fasholatolulope52@gmail.com>
2025-01-07 15:13:49 +0100

    Initial commit

---
+++ view/footer.php
@@ -0,0 +1,9 @@
+	<footer class="main-footer">
+        <div class="pull-right hidden-xs">
+         
+        </div>
+        <strong>Copyright &copy; 2024-2025 project by <a href="">Fashola Tolulope</a>.</strong> All rights reserved.
+     </footer>
+      
+  </body>
+</html>
